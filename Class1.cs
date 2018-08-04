55dfe94e21d7969bdbce8d271c321216a9264da5
BilalMughal <33897726+ansarMalik@users.noreply.github.com>
2018-08-04 06:36:31 -0700

    library added
    library added

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_library
+{
+    public class student
+    {
+        public void show()
+        {
+            Console.WriteLine("show method in student class");
+        }
+    }
+
+    public class customer
+    {
+        public void run()
+        {
+            Console.WriteLine("run method in customer class");
+        }
+    }
+
+    public class khan : customer
+    {
+        static void Main(string[] args)
+        {
+            new customer();
+        }
+    }
+}
